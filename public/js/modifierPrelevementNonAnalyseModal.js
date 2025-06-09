a63b741457ae908152ce48565efc6b3cb687cddb
lucasjgd <163435767+lucasjgd@users.noreply.github.com>
2025-06-09 21:33:56 +0200

    Update modifierPrelevementNonAnalyseModal.js

--- public/js/modifierPrelevementNonAnalyseModal.js
+++ public/js/modifierPrelevementNonAnalyseModal.js
@@ -1,26 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-const modifModal = document.getElementById('modifierPrelevementNonAnalyseModal');
-modifModal.addEventListener('show.bs.modal', function (event) {
-const button = event.relatedTarget;
+    const modifModal = document.getElementById('modifierPrelevementNonAnalyseModal');
+    modifModal.addEventListener('show.bs.modal', function (event) {
+        const button = event.relatedTarget;
 
-const id = button.getAttribute('data-idNonAnalyse');
-const datePrelevementNonAnalyse = button.getAttribute('data-dateNonAnalyse');
-const emplacementIdPrelevementNonAnalyse = button.getAttribute('data-emplacementIdNonAnalyse');
+        const id = button.getAttribute('data-idNonAnalyse');
+        const datePrelevementNonAnalyse = button.getAttribute('data-dateNonAnalyse');
+        const emplacementIdPrelevementNonAnalyse = button.getAttribute('data-emplacementIdNonAnalyse');
 
 
-document.getElementById('modifDatePrelevementNonAnalyse').value = datePrelevementNonAnalyse;
+        document.getElementById('modifDatePrelevementNonAnalyse').value = datePrelevementNonAnalyse;
 
-const emplacementSelect = document.getElementById('modifEmplacementPrelevementNonAnalyse');
-const options = emplacementSelect.options;
-for (let i = 0; i < options.length; i++) {
-if (options[i].value == emplacementIdPrelevementNonAnalyse) {
-options[i].selected = true;
-break;
-}
-}
+        const emplacementSelect = document.getElementById('modifEmplacementPrelevementNonAnalyse');
+        const options = emplacementSelect.options;
+        for (let i = 0; i < options.length; i++) {
+            if (options[i].value == emplacementIdPrelevementNonAnalyse) {
+                options[i].selected = true;
+                break;
+            }
+        }
 
-const form = document.getElementById('modifNonAnalyseForm');
-form.action = `/gestion_prelevement/modif-nonAnalyse/${id}`;
+        const form = document.getElementById('modifNonAnalyseForm');
+        form.action = `/gestion_prelevement/modif-nonAnalyse/${id}`;
 
-});
+    });
 });
